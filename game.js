b62bd74ce60e874d3f81cedc2cfe91662d46f169
Portizhead <fer321nando@gmail.com>
2021-08-15 02:05:30 -0500

    zelda kaboom little update

---
+++ game.js
@@ -0,0 +1,18 @@
+kaboom({
+    global: true,
+    fullscreen: true,
+    scale: 1,
+    debug: true,
+})
+
+loadRoot()
+loadSprite('link-going-left', 'https://imgur.com/1Xq9biB')
+loadSprite('link-going-right', 'https://imgur.com/yZIb8O2');
+//min 7:18
+scene("game",() => {
+
+
+
+});
+
+start("game")
