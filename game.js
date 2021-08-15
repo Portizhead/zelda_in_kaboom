3bb375390280f2e1ad6986e7fab297276e2a7912
Portizhead <fer321nando@gmail.com>
2021-08-15 13:19:26 -0500

    some sprites where added

--- game.js
+++ game.js
@@ -6,8 +6,31 @@ kaboom({
 })
 
 loadRoot()
-loadSprite('link-going-left', 'https://imgur.com/1Xq9biB')
+loadSprite('link-going-left', 'https://imgur.com/1Xq9biB');
 loadSprite('link-going-right', 'https://imgur.com/yZIb8O2');
+loadSprite('link-going-down', '');
+loadSprite('link-going-up', '');
+loadSprite('left-wall');
+loadSprite('right-wall');
+loadSprite('top-wall');
+loadSprite('bottom-wall', 'https://imgur.com/vWJWmvb');
+loadSprite('bottom-left-wall', 'https://imgur.com/awnTfNC');
+loadSprite('bottom-right-wall', 'https://imgur.com/84oyTFy');
+loadSprite('top-left-wall', 'https://imgur.com/xlpUxIm');
+loadSprite('top-right-wall', 'https://imgur.com/z0OmBd1');
+loadSprite('top-door');
+loadSprite('fire-pot', 'https://imgur.com/I7xSp7w');
+loadSprite('left-door', 'https://imgur.com/okdJNls');
+loadSprite('lanterns', 'https://imgur.com/wiSiY09');
+loadSprite('slicer');
+loadSprite('skeletor');
+loadSprite('explosion', 'https://imgur.com/o9WizfI');
+loadSprite('stairs');
+loadSprite('back-ground'), 'https://imgur.com/u4DVsx6';
+loadSprite('grass', 'https://imgur.com/4IzFwwk');
+loadSprite('hole', 'https://imgur.com/jgURLQ2');
+
+
 //min 7:18
 scene("game",() => {
 
